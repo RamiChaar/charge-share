18d070a26ff4271b776a00d317142c3f69b1e21d
RamiChaar <Chaarrami22@gmail.com>
2022-10-04 12:09:39 -0700

    Added Map Fragment to Main Activity

--- app/src/main/java/com/example/evchargingapp/MainActivity.kt
+++ app/src/main/java/com/example/evchargingapp/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.evchargingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Transformations.map
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.evchargingapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker at CSUN and move the camera
+        val csun = LatLng(34.2407, -118.5300)
+        val zoomLevel = 12.0f;
+        mMap.addMarker(MarkerOptions().position(csun).title("Marker in CSUN"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(csun, zoomLevel));
     }
 }
