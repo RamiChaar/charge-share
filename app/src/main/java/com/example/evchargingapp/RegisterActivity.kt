03c88bbc14c17c00d5375fbd2b1c97ccaadbac20
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-11-08 12:47:38 -0800

    Val text val intent on both Login and Register Activity, not sure if both pages are connected now, will go back

--- app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
+++ app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
@@ -1,11 +1,19 @@
 package com.example.evchargingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val text = findViewById<TextView>(R.id.alreadyHaveAccount)
+
+        text.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+        }
     }
 }
