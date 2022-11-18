657888009f2712fb64a6521a78ed1b53a0f864dd
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-11-17 20:23:26 -0800

    move paged from login to register when you click on sign up (red)

--- app/src/main/java/com/example/evchargingapp/LoginActivity.kt
+++ app/src/main/java/com/example/evchargingapp/LoginActivity.kt
@@ -1,17 +1,26 @@
 package com.example.evchargingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.evchargingapp.databinding.ActivityLoginBinding
+import com.example.evchargingapp.databinding.ActivityRegisterBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        val text = findViewById<TextView>(R.id.textViewSignUp)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        text.setOnClickListener {
-
+        binding.btnlogin.setOnClickListener {
         }
+        binding.textViewSignUp.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
     }
 }
