6ba1c66735268d30d8c92782174618eb207311b6
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-11-22 11:15:15 -0800

    Already have an account button working

--- app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
+++ app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
@@ -1,19 +1,26 @@
 package com.example.evchargingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.example.evchargingapp.databinding.ActivityRegisterBinding
+import java.text.AttributedCharacterIterator
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val alreadyHaveAccountBtn = findViewById<TextView>(com.example.evchargingapp.R.id.alreadyHaveAccount)
-
-        alreadyHaveAccountBtn.setOnClickListener {
+        binding.btnRegister.setOnClickListener {
+        }
+        binding.alreadyHaveAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+            }
 
         }
     }
-
-}
