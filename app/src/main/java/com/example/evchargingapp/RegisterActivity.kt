dabd3f7f8547282ff5c7c5330a9a6d06c61161cf
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-11-29 13:04:34 -0800

    Register page connected to database!

--- app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
+++ app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
@@ -4,19 +4,47 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.example.evchargingapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 import java.text.AttributedCharacterIterator
 
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener {
+            val username = binding.inputPass.text.toString()
+            val email = binding.inputEmail.text.toString()
+            val pass = binding.inputPassword.text.toString()
+            val confirmPass = binding.inputConfirmPassword.text.toString()
+
+            if (username.isNotEmpty() && email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email , pass).addOnCompleteListener{
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, LoginActivity::class.java)
+                            startActivity(intent)
+                        }else{
+                            Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(this, "Empty Fields Are not Allowed !", Toast.LENGTH_SHORT).show()
+            }
         }
         binding.alreadyHaveAccount.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
