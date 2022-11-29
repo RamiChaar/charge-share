931a05d344a0560285a77bbcfb8a45f527bf5c3e
Juanxsb <78043641+Juanxsb@users.noreply.github.com>
2022-11-29 13:53:12 -0800

    database connected to login

--- app/src/main/java/com/example/evchargingapp/LoginActivity.kt
+++ app/src/main/java/com/example/evchargingapp/LoginActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import com.example.evchargingapp.databinding.ActivityLoginBinding
 import com.example.evchargingapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +20,25 @@ class LoginActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnlogin.setOnClickListener {
+            val email = binding.inputEmail.text.toString()
+            val pass = binding.inputPassword.text.toString()
+
+
+            if (email.isNotEmpty() && pass.isNotEmpty() ) {
+
+
+                    firebaseAuth.signInWithEmailAndPassword(email , pass).addOnCompleteListener{
+                        if (it.isSuccessful) {
+                            val intent = Intent(this, MainActivity::class.java)
+                        }else{
+                            Toast.makeText(this, it.exception.toString() , Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Empty Fields Are not Allowed !", Toast.LENGTH_SHORT).show()
+                }
+
         }
         binding.textViewSignUp.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
