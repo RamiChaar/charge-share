5fb2704b67d0808554e9d8a03a50b482a0795431
Sol-17 <112975698+Sol-17@users.noreply.github.com>
2022-12-01 11:32:04 -0800

    added welcome page and rentmycharger
    added the welcome page activity and xlm and the rent my home charging station activity and xlm

---
+++ app/src/main/java/com/example/evchargingapp/rentHomeCharging.kt
@@ -0,0 +1,11 @@
+package com.example.evchargingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class rentHomeCharging : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rent_home_charging)
+    }
+}
---
+++ app/src/main/java/com/example/evchargingapp/welcomePage.kt
@@ -0,0 +1,11 @@
+package com.example.evchargingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class welcomePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_page)
+    }
+}
