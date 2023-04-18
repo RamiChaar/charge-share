3cc2268d57dafb0c1f6e9a52770535cf7d4cd148
RamiChaar <Chaarrami22@gmail.com>
2023-04-18 12:30:39 -0700

    Update RegisterActivity.kt

--- app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
+++ app/src/main/java/com/example/evchargingapp/RegisterActivity.kt
@@ -18,6 +18,7 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
         firebaseAuth = FirebaseAuth.getInstance()
 
