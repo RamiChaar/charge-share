2c01a40e0a1eee0972fc2b3c8589effcc7b63610
Sol-17 <112975698+Sol-17@users.noreply.github.com>
2023-04-25 10:56:56 -0700

    added Report Activity
    still need to add back end code and make it look better

---
+++ app/src/main/java/com/example/evchargingapp/ReportActivity.kt
@@ -0,0 +1,11 @@
+package com.example.evchargingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
